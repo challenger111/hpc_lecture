e15aecc1f32b5f11c17e8af03659058560025f32
challenger111 <58408817+challenger111@users.noreply.github.com>
2020-05-14 09:48:01 +0800

    Update 14_bucket_sort.cpp

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include<omp.h>
 
 int main() {
   int n = 50;
@@ -13,14 +14,19 @@ int main() {
   printf("\n");
 
   std::vector<int> bucket(range); 
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
     bucket[i] = 0;
   }
-  for (int i=0; i<n; i++) {
+#pragma omp parallel for shared(bucket)
+  for (int i =0; i<n; i++) {
+#pragma omp parallel for shared(bucket)
     bucket[key[i]]++;
   }
   for (int i=0, j=0; i<range; i++) {
+#pragma omp parallel for shared(key)
     for (; bucket[i]>0; bucket[i]--) {
+#pragma omp parallel for shared(bucket)
       key[j++] = i;
     }
   }
