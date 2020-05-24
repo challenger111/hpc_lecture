700a8e99a3d03956967646bd592b38096af8416d
challenger111 <58408817+challenger111@users.noreply.github.com>
2020-05-24 22:30:19 +0800

    Update 12_bucket_sort.cpp

--- 05_cuda/12_bucket_sort.cpp
+++ 05_cuda/12_bucket_sort.cpp
@@ -2,31 +2,47 @@
 #include <cstdlib>
 #include <vector>
 
+
+__global__ void initial_0(int *bucket) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	bucket[i] = 0;
+}
+
+__global__ void counter(int *bucket,int *key) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	atomicAdd(&bucket[key[i], 1);
+}
+
+__global__ void recount(int *bucket, int *key) {
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i >= 50)
+		return;
+	for (int j = 0, k = 0; j <= i; k++) {
+		key[i] = k;
+		j += bucket[k];
+	}
+}
+
 int main() {
-  int n = 50;
-  int range = 5;
-  std::vector<int> key(n);
-  for (int i=0; i<n; i++) {
-    key[i] = rand() % range;
-    printf("%d ",key[i]);
-  }
-  printf("\n");
+	int n = 50;
+	int range = 5;
+	int *key;
+	int *bucket;
+	cudaMalloc(&key, n * sizeof(int));
+	cudaMalloc(&bucket, range * sizeof(int));
+	for (int i = 0; i < n; i++) {
+		key[i] = rand() % range;
+		printf("%d ", key[i]);
+	}
+	printf("\n");
 
-  std::vector<int> bucket(range); 
-  for (int i=0; i<range; i++) {
-    bucket[i] = 0;
-  }
-  for (int i=0; i<n; i++) {
-    bucket[key[i]]++;
-  }
-  for (int i=0, j=0; i<range; i++) {
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
-    }
-  }
+	initial_0 <<<1, range >>> (bucket);
+	counter <<<1, n >>> (bucket, key);
+	recounter <<<(n + 32 - 1) / 32, 32, range >>> (bucket, key);
 
-  for (int i=0; i<n; i++) {
-    printf("%d ",key[i]);
-  }
-  printf("\n");
+
+	for (int i = 0; i < n; i++) {
+		printf("%d ", key[i]);
+	}
+	printf("\n");
 }
